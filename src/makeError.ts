0030557cff9cec0206bf920f3f6527c5cc556028
Andy Edwards <jedwards@fastmail.com>
2020-07-13 01:04:58 -0500

    fix(makeError): fix type error

--- src/makeError.ts
+++ src/makeError.ts
@@ -7,6 +7,7 @@ export default function makeError(
   input: any
 ): TypeError | null | undefined {
   const validation = new Validation(input)
-  validation.errors = Array.from(expected.errors(validation, [], input))
+  for (const error of expected.errors(validation, [], input))
+    validation.errors.push(error)
   return makeTypeError(validation)
 }
